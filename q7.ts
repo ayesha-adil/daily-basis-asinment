2a9c87e435c8380b71b7380075a4d1f6530441bc
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-03-18 08:46:54 +0500

    Add files via upload

---
+++ q7.ts
@@ -0,0 +1,10 @@
+let fanDetail = {
+    color:'white and black',
+    quailty:'good',
+    speed:"ok",
+    company:'pak fan',
+    serialNumbe:23455,
+    condition:'very good',
+    fanName:'pak fan',
+}
+console.log(fanDetail)
