fa814b380d6832b07a7893c308a30c9e8aa7564c
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-03-18 08:46:30 +0500

    Add files via upload

---
+++ q6.ts
@@ -0,0 +1,19 @@
+let purseDetail = {
+color:"black",
+pocket:2,
+belt:2,
+desgin:"flowers",
+companyName: "pak",
+product:"Made in paklistan",
+condition:"news",
+isGrunty:false }
+
+console.log(purseDetail)
+
+
+
+
+
+
+
+
