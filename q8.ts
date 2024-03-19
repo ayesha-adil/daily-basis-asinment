261fee3261c5b393602cb989b13955ce304b726a
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-03-19 08:10:48 +0500

    Add files via upload

---
+++ q8.ts
@@ -0,0 +1,36 @@
+let studentsName : {
+name:string,
+class:string,
+vaccinated:boolean,
+age:number,
+classMoniter:boolean,
+fovouritSubject:string,
+// example 01
+}={
+name:'ayesha',
+class:'eight',
+vaccinated:true,
+age:25,
+classMoniter:true,
+fovouritSubject:'urdu'
+// example 02
+}
+let student02 = {
+    name:'isha',
+    class:'eight',
+    vaccinated:true,
+    age:23,
+    classMoniter:false,
+    fovouritSubject:'english'}
+    
+// example 03
+
+let student03 = {
+name:'ali',
+house:2136,
+streetNo:11,
+bastiName:'azam basti'}
+console.log(studentsName,student02,student03)
+
+
+
