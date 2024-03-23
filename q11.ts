afb15f573b98ff5ae00e37742342d40d1d0ab273
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-03-23 10:37:53 +0500

    Add files via upload

---
+++ q11.ts
@@ -0,0 +1,19 @@
+// interface
+let biryani ={
+oil:"2cup",
+onion:2,
+greenChili:"3",
+tamato:5,
+water:"10glass",
+salt:"1spon",
+blackPeper:"2spon",
+whitepeper:"1spon",
+rice:"5glass",
+biryanimesala:"1spon"
+
+};
+console.log(biryani)
+
+
+
+
