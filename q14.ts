f39fd7fdf753c25a06dd314fcc2b27dc3a9d55de
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-03-27 08:43:05 +0500

    Add files via upload

---
+++ q14.ts
@@ -0,0 +1,11 @@
+// custered
+// 1 Milk, 3 Sugar,4Powder ;
+function custered ():number{
+    console.log(3.5+1+4)
+    let cooked=1+3+4
+return cooked
+
+ }
+ custered() 
+custered()
+let cooked1 :number= custered()
---
+++ q15.ts
@@ -0,0 +1,35 @@
+function Addition():number{
+    let total=3+5
+    return total
+}
+Addition()
+let number1:number= Addition();
+console.log(number1)
+// return funcation 2
+function person():string{
+    let name = "ayesha"
+    return name
+}
+person()
+let person1:string = person()
+console.log(person1)
+function city():string{
+    let name= "karachi"
+    return name
+}
+city()
+let name1 : string= city()
+console.log(name1)
+// funcation number
+function noddle(water,noddle,mesala){
+    console.log(2+1+1)
+}
+noddle(2,1,1)
+
+
+function noddle1():number{
+    let cooked = noddle
+    return noddle
+}
+noddle( 2,1,1)
+let cooked :any = noddle1()
