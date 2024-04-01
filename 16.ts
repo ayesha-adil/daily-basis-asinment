893c00940d7b002445fec81fe150e6a458af8353
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-04-02 01:15:41 +0500

    Add files via upload

---
+++ 16.ts
@@ -0,0 +1,35 @@
+function tea(water,milk,suger,lapton){
+console.log(1+1+2+1)
+
+}
+tea(1,1,2,1)
+function tea2(){
+    let cooked=tea
+    return tea
+}
+
+    tea(1,1,2,1)
+let tea1:any = tea2()
+
+function coffee(milk,suger,powder){
+  console.log(1+0+1)  
+}
+coffee(1,0,1)
+function coffee1(){
+    let cooked=coffee
+    return coffee
+
+}
+coffee(1,0,1)
+let cooked1:any=coffee1()
+function chat(){
+    console.log("chany+ononline+patato+chatmesala")
+}
+chat()
+function chat1(){
+    let maked=chat
+    return chat
+
+}
+
+let chat2:any=chat()
